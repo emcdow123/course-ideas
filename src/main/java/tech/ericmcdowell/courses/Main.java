dff1c35d6d0345e7a0882647a8d2c4fa1717ca96
Eric McDowell <emcdow123@gmail.com>
2017-02-14 00:32:29 -0500

    Initial commit

---
+++ src/main/java/tech/ericmcdowell/courses/Main.java
@@ -0,0 +1,34 @@
+package tech.ericmcdowell.courses;
+
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.get;
+import static spark.Spark.post;
+
+/**
+ * Created by eric on 2/13/17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        get("/", (req, res) -> {
+            Map<String, String> model = new HashMap<>();
+            model.put("username", req.cookie("username"));
+
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/sign-in",(req, res) ->{
+            Map<String, String> model = new HashMap<>();
+            String username = req.queryParams("username");
+            model.put("username", username);
+            res.cookie("username", username);
+
+
+            return new ModelAndView(model, "sign-in.hbs");
+        }, new HandlebarsTemplateEngine());
+    }
+}
