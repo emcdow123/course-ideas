1394825e6c8df169ea6295e046b8cbed44bf7351
Eric McDowell <eric.mcdowell@bhsi.com>
2017-02-16 16:23:51 -0500

    Added functionality to add courses, added CSS, setup restrictions for access

--- src/main/java/tech/ericmcdowell/courses/Main.java
+++ src/main/java/tech/ericmcdowell/courses/Main.java
@@ -10,9 +10,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static spark.Spark.get;
-import static spark.Spark.post;
-import static spark.Spark.staticFileLocation;
+import static spark.Spark.*;
 
 /**
  * Created by eric on 2/13/17.
@@ -23,9 +21,23 @@ public class Main {
 
         CourseIdeaDAO dao = new SimpleCourseIdeaDAO();
 
+        before((req, res) -> {
+            if(req.cookie("username") != null) {
+                req.attribute("username", req.cookie("username"));
+            }
+        });
+
+        before("/ideas", (req, res) ->{
+            // TODO:ETM - Send a message to the user about the redirect somehow
+            if(req.attribute("username") == null) {
+                res.redirect("/");
+                halt();
+            }
+        });
+
         get("/", (req, res) -> {
             Map<String, String> model = new HashMap<>();
-            model.put("username", req.cookie("username"));
+            model.put("username", req.attribute("username"));
 
             return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
@@ -38,5 +50,19 @@ public class Main {
 
             return new ModelAndView(model, "sign-in.hbs");
         }, new HandlebarsTemplateEngine());
+
+        get("/ideas", (req, res) ->{
+            Map<String, Object> model = new HashMap<>();
+            model.put("ideas", dao.findAll());
+            return new ModelAndView(model, "ideas.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/ideas", (req, res) ->{
+            String title = req.queryParams("title");
+            CourseIdea courseIdea = new CourseIdea(title, req.attribute("username"));
+            dao.add(courseIdea);
+            res.redirect(("/ideas"));
+            return null;
+        });
     }
 }
